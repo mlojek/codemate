e17c1791c824cd00f76e7999f964f1bc7e4f872c
Marcin <mlojek@protonmail.com>
2025-02-15 22:20:56 +0100

    Add llm integration using langchain

--- src/codemate/__main__.py
+++ src/codemate/__main__.py
@@ -3,7 +3,13 @@ CLI entrypoint for codemate.
 """
 
 import argparse
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+from langchain.llms.openai import OpenAI
+from langchain.schema import HumanMessage
+from langchain_openai import ChatOpenAI
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -17,9 +23,40 @@ if __name__ == "__main__":
     parser.add_argument(
         "source_file", type=Path, help="Path to the source file to work on."
     )
-    # parser.add_argument('--from_line')
-    # parser.add_argument('--to_line')
-    # parser.add_argument('--inplace')
+    parser.add_argument(
+        "--from_line", help="The line number in the source file to start from."
+    )
+    parser.add_argument(
+        "--to_line", help="The line number in the source file to end at."
+    )
+    parser.add_argument(
+        "--inplace",
+        action="store_true",
+        help="If used the result will be pasted into the source file instead of the console.",
+    )
     args = parser.parse_args()
 
     print(f"Selected option {args.action} on file {args.source_file}")
+
+    # read the configfile
+    status = load_dotenv(Path.home() / ".codemate")
+    print(status)
+    print(os.getenv("MESSAGE"))
+
+    # create prompt template and llm instance
+    # fav_llm in config, flag to select llm
+
+    # llm = ChatOpenAI(model="gpt-4o", api_key=os.getenv('OPENAI_API_KEY'))
+    from langchain.schema import HumanMessage
+    from langchain_google_genai import ChatGoogleGenerativeAI
+
+    # Set up Gemini model in LangChain
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-1.5-pro", google_api_key=os.getenv("GEMINI_API_KEY")
+    )
+    response = llm.invoke([HumanMessage(content="Hello, what's your name?")])
+    print(response.content)
+
+    # read the source file
+    # prompt the llm
+    # append the result to the source file
