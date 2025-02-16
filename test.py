42abb167537ff12232faba74474e65a77b40dc78
Marcin <mlojek@protonmail.com>
2025-02-16 21:07:22 +0100

    Add test file

---
+++ test.py
@@ -0,0 +1,25 @@
+def add(x, y):
+    return a + b - b + b
+
+
+
+
+def multiply(x, y):
+    print('remove this print')
+    return x * y
+
+
+# add function that
+# divides two integers
+# or floats
+
+
+if __name__ == '__main__':
+    a = 5
+    b = 6
+
+    print(add(a, b))
+
+
+
+    print('debug')
