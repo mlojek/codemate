703b12d780c3066dad93effc8e41f982608d3992
Marcin <mlojek@protonmail.com>
2025-02-16 11:14:16 +0100

    Prompting works

--- src/codemate/__main__.py
+++ src/codemate/__main__.py
@@ -7,6 +7,7 @@ import os
 from pathlib import Path
 
 from dotenv import load_dotenv
+from langchain import PromptTemplate
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
@@ -24,10 +25,10 @@ if __name__ == "__main__":
         "source_file", type=Path, help="Path to the source file to work on."
     )
     parser.add_argument(
-        "--from_line", help="The line number in the source file to start from."
+        "--from_line", default=0, help="The line number in the source file to start from."
     )
     parser.add_argument(
-        "--to_line", help="The line number in the source file to end at."
+        "--to_line", default=-1, help="The line number in the source file to end at."
     )
     parser.add_argument(
         "--api",
@@ -49,9 +50,7 @@ if __name__ == "__main__":
         Path.home() / ".codemate"
     ), "Could not find ~/.codemate config file. Run codemate init!"
 
-    # create prompt template and llm instance
     # fav_llm in config
-
     match args.api:
         case "gemini":
             llm = ChatGoogleGenerativeAI(
@@ -65,15 +64,33 @@ if __name__ == "__main__":
         source_file_content = file_handle.read().split("\n")
     print(source_file_content)
 
+    # create prompt template
+    system_message_template = PromptTemplate.from_template(
+        "You are a bot that performs operations on source code. Your task is to {detailed_instruction} the source code. You shall return only source code and no comments.",
+    )
+
+    instructions = {
+        'refactor': 'refactor',
+        "document": 'add docstrings and typehints to',
+        'explain': 'add explaination comments to'
+    }
+
+    messages = [
+        SystemMessage(content=system_message_template.format(detailed_instruction=instructions[args.action])),
+        HumanMessage(
+            content=source_file_content[args.from_line: args.to_line]
+        ),
+    ]
+
     # prompt the llm
-    llm_response = llm.invoke([HumanMessage(content="Hello, what's your name?")])
+    llm_response = llm.invoke(messages)
 
     # append the result to the source file
     if args.inplace:
         source_file_new_content = (
             source_file_content[: args.from_line]
             + llm_response.content
-            + source_file_content[args.to_line + 1 :]
+            + source_file_content[args.to_line:]
         )
         # TODO append response content to source file
         pass
